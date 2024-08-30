6954e6f6933513df5006fc08e1e9f421eccab11c
daisuke-w <w.daisuke.001@gmail.com>
2024-08-30 18:59:43 +0900

    Initial commit

---
+++ src/test/java/d/com/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package d.com.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
