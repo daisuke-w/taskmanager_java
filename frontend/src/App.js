31e907ba578f16a8a14216f5d133ff3e51bfd5ca
daisuke-w <w.daisuke.001@gmail.com>
2024-09-01 12:20:14 +0900

    ルーティング設定

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<TaskList />} />
+                <Route path="/task/new" element={<TaskForm />} />
+                <Route path="/task/edit/:id" element={<TaskForm />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
